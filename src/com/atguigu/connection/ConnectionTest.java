2eb033b12531436f4c6dd69e761d4e734b328150
DELL <347091231@qq.com>
2021-03-25 20:55:22 +0800

    2021-3-25 20:54:55 Company Commit+Push 更新说明:1.0.3: 07-获取数据库连接方式一，第一步

---
+++ src/com/atguigu/connection/ConnectionTest.java
@@ -0,0 +1,40 @@
+package com.atguigu.connection;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.SQLException;
+import java.util.Properties;
+
+//JDBC学习 07-获取数据库连接方式一
+public class ConnectionTest {
+    @Test
+    public void testConnection1() throws SQLException {
+//        声明的是一个driver MySQL的驱动叫什么
+//        对 Driver 进行 ctrl + H 唤起列表
+//        Driver driver = null;
+        Driver driver = new com.mysql.jdbc.Driver();
+//        String url = null;
+//        url:http://loaclhost:8080/gmall/keyboard.jpg
+//        jdbc 主协议 mysql 子协议
+//        jdbc:mysql: 协议
+//        localhost: ip地址
+//        3306: 默认的端口号
+//        book:数据库
+        String url = "jdbc:mysql://localhost:3306/book?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+//        Properties info =null;
+//        对象先做出来
+//        将用户名和密码封装在 Properties 中
+        Properties info = new Properties();
+//        info.setProperty(key,value)
+//        user:用户名 password:密码
+        info.setProperty("user","root");
+        info.setProperty("password","123");
+        //        先定义为null,当你有了具体值
+//        这是一个连接
+        Connection conn = driver.connect(url,info);
+//        看下是不是真的拿到了，打印一下
+        System.out.println(conn);
+    }
+}
---
+++ src/com/atguigui4/exer/Exer1Test.java
@@ -0,0 +1,5 @@
+package com.atguigui4.exer;
+
+public class Exer1Test {
+//    public void updata(String sql,Object...args)
+}
